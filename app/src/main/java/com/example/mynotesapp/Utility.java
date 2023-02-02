4109bf3807f3b5ae16a58d9f53b9ddc12d292c21
sahilsharma17 <sahils17sam@gmail.com>
2023-02-02 10:48:22 +0530

    commit V7

--- app/src/main/java/com/example/mynotesapp/Utility.java
+++ app/src/main/java/com/example/mynotesapp/Utility.java
@@ -3,8 +3,25 @@ package com.example.mynotesapp;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+
 public class Utility {
     static void showToast(Context context,String msg){
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    static CollectionReference getCollectionRefForNotes(){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid()).collection("my_notes");
+    }
+    static String timestampToString(Timestamp timestamp){
+        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
     }
 }
